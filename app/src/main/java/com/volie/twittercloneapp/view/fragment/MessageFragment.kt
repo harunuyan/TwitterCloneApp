a4800eec5979f7471505b0e98aedb7b740b46b33
monster <harunuyan6@gmail.com>
2023-03-20 20:30:07 +0300

    implemented fragment

---
+++ app/src/main/java/com/volie/twittercloneapp/view/fragment/MessageFragment.kt
@@ -0,0 +1,29 @@
+package com.volie.twittercloneapp.view.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.volie.twittercloneapp.databinding.FragmentMessageBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MessageFragment : Fragment() {
+    private var _mBinding: FragmentMessageBinding? = null
+    private val mBinding get() = _mBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _mBinding = FragmentMessageBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _mBinding = null
+    }
+}
---
+++ app/src/main/java/com/volie/twittercloneapp/view/fragment/SearchFragment.kt
@@ -0,0 +1,29 @@
+package com.volie.twittercloneapp.view.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.volie.twittercloneapp.databinding.FragmentSearchBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SearchFragment : Fragment() {
+    private var _mBinding: FragmentSearchBinding? = null
+    private val mBinding get() = _mBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _mBinding = FragmentSearchBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _mBinding = null
+    }
+}
