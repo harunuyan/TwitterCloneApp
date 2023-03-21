1cbc41df1eb2d54f0a073642faee87f9c00841a3
monster <harunuyan6@gmail.com>
2023-03-21 13:04:32 +0300

    implemented navigation drawer view & toolbar

--- app/src/main/java/com/volie/twittercloneapp/view/MainActivity.kt
+++ app/src/main/java/com/volie/twittercloneapp/view/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.volie.twittercloneapp.view
 
 import android.os.Bundle
+import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.elevation.SurfaceColors
 import com.volie.twittercloneapp.R
@@ -20,26 +24,58 @@ class MainActivity : AppCompatActivity() {
         _mBinding = ActivityMainBinding.inflate(layoutInflater)
         setStatusAndNavBarColor()
         setContentView(mBinding.root)
+
+        onBackPressedDispatcher.addCallback(this) {
+            if (mBinding.drawerLayout.isOpen) {
+                mBinding.drawerLayout.close()
+            } else {
+                navController.navigateUp() || kotlin.run {
+                    finish()
+                    true
+                }
+            }
+        }
+        setSupportActionBar(mBinding.toolbar)
         setupBottomNavigation()
+        setupDrawerLayout()
+    }
+
+    private fun setupDrawerLayout() {
+        val appBarConfiguration = AppBarConfiguration(navController.graph, mBinding.drawerLayout)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        mBinding.navigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     private fun setupBottomNavigation() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
         mBinding.bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController)
     }
 
-    fun setStatusAndNavBarColor() {
+    private fun setStatusAndNavBarColor() {
         val window = window
         val color = SurfaceColors.SURFACE_2.getColor(this)
         window!!.statusBarColor = color
         window.navigationBarColor = color
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+    override fun onBackPressed() {
+        super.getOnBackPressedDispatcher().onBackPressed()
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        if (mBinding.drawerLayout.isOpen) {
+            mBinding.drawerLayout.close()
+            return true
+        }
+        return NavigationUI.navigateUp(navController, mBinding.drawerLayout)
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
